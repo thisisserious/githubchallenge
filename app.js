2e45de40d9d05bd95890f849c1398832a221edc0
Laura Abend <laura.l.abend@gmail.com>
2016-10-03 17:15:42 -0500

    update app.js

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 console.log('Up & running!');
 
 // FizzBuzz Challenge
-var num = 100;
-for (var i = 1; i <= 100; i++) {
-  if (i % 3 === 0 && i % 5 !== 0) {
-    console.log('Fizz');
-  } else if (i % 5 === 0 && i % 3 !== 0) {
-    console.log('Buzz');
-  } else if (i % 3 === 0 && i % 5 === 0) {
-    console.log('FizzBuzz');
-  } else {
-    console.log(i);
-  }
-}
+// var num = 100;
+// for (var i = 1; i <= 100; i++) {
+//   if (i % 3 === 0 && i % 5 !== 0) {
+//     console.log('Fizz');
+//   } else if (i % 5 === 0 && i % 3 !== 0) {
+//     console.log('Buzz');
+//   } else if (i % 3 === 0 && i % 5 === 0) {
+//     console.log('FizzBuzz');
+//   } else {
+//     console.log(i);
+//   }
+// }
 
 $(function () {
   $('button').on('click', function () {

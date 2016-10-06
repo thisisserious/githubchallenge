963a45d77b4374a44068fa1b53db564d586bec26
Laura Abend <laura.l.abend@gmail.com>
2016-10-05 21:46:05 -0500

    add reverse practice function and a broken button append

--- practice.js
+++ practice.js
@@ -9,4 +9,13 @@ function reverseNumber(n) {
   return n.split('').reverse().join('');
 }
 
+var newNum = reverseNumber(32243);
 console.log(reverseNumber(32243));
+
+$('button').on('click', function () {
+  if (newNum == 34223) {
+    $('#greeting').append('<div id="affirmation"></div>');
+  } else {
+    $('#greeting').append('<div id="fail"></div>');
+  }
+});

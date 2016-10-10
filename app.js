b281045db239c94f034f3c7568aa34b2beeb2fa6
Laura Abend <laura.l.abend@gmail.com>
2016-10-09 20:33:48 -0500

    add append function

--- app.js
+++ app.js
@@ -3,5 +3,8 @@ console.log('app.js working');
 $(function () {
   $('button').on('click', function () {
     console.log('Button clicked!');
+    var $div = ('<div></div>');
+    $div.append('<p>I am</p>');
+    console.log($div);
   });
 });
